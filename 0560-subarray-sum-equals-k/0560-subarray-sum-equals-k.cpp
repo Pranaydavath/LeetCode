612d2c6b1774987819adc549395dbf991014076e
Davath Pranay <73703892+Pranaydavath@users.noreply.github.com>
2024-01-02 14:42:31 +0530

    Time: 176 ms (5.11%), Space: 47.6 MB (20.18%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,9 +4,9 @@ public:
          int n=v.size();
         int ans=0,sum=0;
         unordered_map<int,int>mp;
-        mp[0]=1;//For empty subarray 
         for(int i=0;i<n;i++){
             sum+=v[i];
+            if(sum==k)ans+=1;
             int dif=sum-k;
             ans+=mp[dif];
             mp[sum]+=1;
