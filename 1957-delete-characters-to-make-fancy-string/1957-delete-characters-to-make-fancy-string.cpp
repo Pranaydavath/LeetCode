6564aa8624a29b44d00e84db768051c824791e9c
Davath Pranay <73703892+Pranaydavath@users.noreply.github.com>
2024-11-01 22:29:50 +0530

    Time: 30 ms (44.16%), Space: 42.9 MB (30.23%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-         s+='^';
      int n=s.size();
      string res="";
+    res.push_back(s[0]);
      int c=1;
     for(int i=1;i<n;i++){
-        if(s[i]!=s[i-1]){
-            c=1;
-          res+=s[i-1];
+        if(s[i]==res.back()){
+            c++;
+            if(c<3){
+                res.push_back(s[i]);
+            }
         }
         else{
-            c+=1;
-            res+=s[i-1];
-            if(c==3){
-               while(s[i]==s[i-1]){
-                   i++;
-               }
-               c=1;
-            }
-            
+            c=1;
+            res.push_back(s[i]);
         }
     }
     
-return res;
+    return res;
     }
 };
