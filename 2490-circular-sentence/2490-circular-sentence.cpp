c7f4659e8b3e737c729aab0173f48c7a29798dec
Davath Pranay <73703892+Pranaydavath@users.noreply.github.com>
2024-11-02 22:52:53 +0530

    Time: 3 ms (10.00%), Space: 9.6 MB (6.09%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isCircularSentence(string v) {
+        char ch=v[0];
+        v+=" ";
+        vector<string> res;
+        int i=0;
+        for(i=0;i<v.size();i++){
+             string r="";
+            while(i<v.size()&&v[i]!=' '){
+                r+=v[i];
+                i++;
+            }
+            res.push_back(r);
+        }
+        int n=res.size();
+        if(n==1){
+            int x=res[0].size();
+            if(res[0][0]==res[0][x-1]) return true;
+            return false;
+        }
+        else{
+           for(int i=1;i<n;i++){
+               int x=res[i].size();
+               int y=res[i-1].size();
+               if(res[i][0]!=res[i-1][y-1])return false;
+           }
+            if(res[0][0]==res[n-1][res[n-1].size()-1])return true;
+        return false;
+        }
+        // for(int i=0;i<res.size();i++){
+        //     cout<<res[i]<<endl;
+        // }
+       
+    }
+};
